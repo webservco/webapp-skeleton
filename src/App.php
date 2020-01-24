8bd06a1248791556d28b6bfa09fa0f1e8ad15ec0
Radu Maier <ariademur@webserv.co>
2020-01-24 17:48:26 +0100

    WSFW App

--- src/App.php
+++ src/App.php
@@ -1,10 +1,11 @@
 <?php
 namespace Project;
 
-use WebServCo\Framework\Log\FileLogger;
-
-final class App extends \WebServCo\Framework\Application
+final class App extends \WebServCo\Framework\App
 {
+    /*
+    * Needs to be inside the project becasue of the __NAMESPACE__ usage.
+    */
     public function __construct($pathPublic, $pathProject = null)
     {
         /**
@@ -16,48 +17,5 @@ final class App extends \WebServCo\Framework\Application
         parent::__construct($pathPublic, $pathProject, __NAMESPACE__);
 
         $this->config()->set('app/path/log', sprintf('%svar/log/', $this->projectPath));
-    }
-
-    /**
-     * Handle HTTP errors.
-     */
-    protected function haltHttp($errorInfo = [])
-    {
-        $this->logError($errorInfo, false);
-        return parent::haltHttp($errorInfo);
-    }
-
-    /**
-     * Handle CLI errors
-     */
-    protected function haltCli($errorInfo = [])
-    {
-        $this->logError($errorInfo, true);
-        return parent::haltCli($errorInfo);
-    }
-
-    protected function logError($errorInfo, $isCli = false)
-    {
-        $logger = new FileLogger(
-            sprintf('error%s', $isCli ? 'CLI' : ''),
-            $this->config()->get('app/path/log'),
-            $this->request()
-        );
-        $errorMessage = sprintf('Error: %s in %s:%s', $errorInfo['message'], $errorInfo['file'], $errorInfo['line']);
-        if ($errorInfo['exception'] instanceof \Exception) {
-            $previous = $errorInfo['exception']->getPrevious();
-            if ($previous instanceof \Exception) {
-                do {
-                    $errorMessage .= sprintf(
-                        '%sPrevious: %s in %s:%s',
-                        PHP_EOL,
-                        $previous->getMessage(),
-                        $previous->getFile(),
-                        $previous->getLine()
-                    );
-                } while ($previous = $previous->getPrevious());
-            }
-        }
-        $logger->error($errorMessage, []);
     }
 }
