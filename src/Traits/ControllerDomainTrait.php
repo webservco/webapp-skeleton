3197a5d834fb8f7e13737f2e86359f327b500071
Radu Maier <ariademur@webserv.co>
2020-02-27 15:20:37 +0100

    abstract method

--- src/Traits/ControllerDomainTrait.php
+++ src/Traits/ControllerDomainTrait.php
@@ -7,6 +7,7 @@ trait ControllerDomainTrait
 {
     abstract protected function config();
     abstract public function data($key, $defaultValue = false);
+    abstract protected function request();
     abstract protected function setData($key, $value);
 
     protected function initDomain()
